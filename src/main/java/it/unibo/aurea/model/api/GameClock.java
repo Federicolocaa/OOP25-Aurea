1a3908b1513397ad47e184db9d602abda68421e2
Nikita Piraino <nikita.piraino@studio.unibo.it>
2026-03-18 11:12:40 +0100

    adapted for checkstyle the interface CameClock

--- src/main/java/it/unibo/aurea/model/api/GameClock.java
+++ src/main/java/it/unibo/aurea/model/api/GameClock.java
@@ -1,45 +1,37 @@
 package it.unibo.aurea.model.api;
 
 /** 
- * Manage the time durng the game.
- * 
+ * Manage the time during the game.
  */
 public interface GameClock {
     /**
-     * 
-     * @return how many cards were already presented to the player
-     * the number start from 0. 
+     * @return how many cards were already presented to the player the number start from 0. 
      * 
      */
     int getCardIndex();
 
     /**
-     * 
      * @return how many cards are presented for semester to the player
      */
     int getCardsPerSemester();
 
     /**
-     * 
-     * increase the number of card already shown to the player
+     * increase the number of card already shown to the player.
      */
     void increaseCardIndex();
 
     /**
-     * 
-     * @return the current semester
+     * @return the current semester.
      */
     int getCurrentSemester();
 
     /**
-     * 
      * @return how many semester are played in a game.
      */
     int getSemestersPerGame();
 
     /**
-     * 
-     * increase the numer of semsters palyed by the player
+     * increase the numer of semeters played by the player.
      */
     void increaseCurrentSemester();
 }
