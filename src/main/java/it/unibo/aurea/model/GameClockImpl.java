105e49a20fa6b080b9e29b5f900ddae9ccbfc2ef
Nikita Piraino <nikita.piraino@studio.unibo.it>
2026-03-27 13:25:14 +0100

    i've started the implementation of gameClock and started the next turn method, as consequense i've created some private methods for the conditions

--- src/main/java/it/unibo/aurea/model/GameClockImpl.java
+++ src/main/java/it/unibo/aurea/model/GameClockImpl.java
@@ -1,27 +1,62 @@
 package it.unibo.aurea.model;
 
+import java.util.Objects;
+import java.util.concurrent.locks.Condition;
+
 import it.unibo.aurea.model.api.GameClock;
+import it.unibo.aurea.model.api.GameConfig;
 
 /**
  * imlmentation of the GameClock.
  */
 public final class GameClockImpl implements GameClock {
+    private final GameConfig gameConfiguration;
+    //we start from 0 with the counter.
+    private int currentTurn = 0;
+    private int currentSemester = 0;
+    private boolean timeFinished = false;
+
+    public GameClockImpl(GameConfig gameConfiguration) {
+        this.gameConfiguration = Objects.requireNonNull(gameConfiguration);
+    }
+
+
+    @Override
+    public void nextTurn() {
+        if (timeFinished) {
+            throw new IllegalStateException("Game already finished");
+        }
+
+        if (hasNextTurnInSemester()) {
+            currentTurn++;
+        } else if (hasNextSemester()) {
+            currentSemester++;
+            currentTurn = 0;
+        } else {
+            timeFinished = true;
+        }
+    }
+
+    private boolean hasNextSemester() {
+        return this.currentTurn +1 < this.gameConfiguration.getCardsPerSemester();
+    }
+
+    private boolean hasNextTurnInSemester() {
+        return this.currentSemester +1 < this.gameConfiguration.getSemestersPerGame();
+    }
+
+    //GETTERS
+    public boolean isTimeFinished() {
+        return timeFinished;
+    }
 
     @Override
     public int getCurrentSemester() {
-        // TODO Auto-generated method stub
-        return 0;
+        return currentSemester;
     }
 
     @Override
     public int getCurrentTurn() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    @Override
-    public void nextTurn() {
-        // TODO Auto-generated method stub
-
+        return currentTurn;
     }
 }
