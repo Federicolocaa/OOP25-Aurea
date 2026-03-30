255d4a1018e1fca716a1893711a523b4025bf5fa
Pietro Dapporto <pietro.dapporto@studio.unibo.it>
2026-03-30 18:21:52 +0200

    added cards in .yaml for testing the loading into class Deck

--- src/test/java/it/unibo/aurea/CardLoaderTest.java
+++ src/test/java/it/unibo/aurea/CardLoaderTest.java
@@ -2,15 +2,17 @@ package it.unibo.aurea;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.io.IOException;
 import java.io.InputStream;
+
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
+import it.unibo.aurea.model.Deck;
+import it.unibo.aurea.model.api.Card;
 import it.unibo.aurea.model.api.CharacterType;
 import it.unibo.aurea.model.api.ParameterType;
 import it.unibo.aurea.model.dto.CardDTO;
@@ -20,24 +22,42 @@ import it.unibo.aurea.model.dto.CardsFile;
  * Small test to ensure loading cards from .yaml file works.
  */
 class CardLoaderTest {
+    private static final int MEDIUM_CHANGE = 7;
+    private static final int ACTUAL_SIZE = 4;
+
     @Test
     void shouldLoadCardsFromYaml() throws IOException {
         final InputStream input = getClass().getClassLoader().getResourceAsStream("cards.yaml");
         assertNotNull(input, "cards.yaml should exist");
 
         final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-
         final CardsFile file = mapper.readValue(input, CardsFile.class);
 
         assertNotNull(file);
         assertNotNull(file.cards());
-        assertFalse(file.cards().isEmpty());
+        assertEquals(ACTUAL_SIZE, file.cards().size());
 
         final CardDTO first = file.cards().get(0);
         assertEquals(CharacterType.PROFESSOR, first.character());
-        assertEquals(first.refusal().effects().getFirst().parameter(), ParameterType.PROFESSORS);
-        assertEquals(first.refusal().effects().getLast().parameter(), ParameterType.STUDENTS);
+        assertEquals(ParameterType.PROFESSORS, first.refusal().effects().getFirst().parameter());
+        assertEquals("No, the budget is too tight", first.refusal().text());
         assertNotNull(first.approval());
+        final CardDTO third = file.cards().get(2);
+        assertEquals(CharacterType.MUM, third.character());
+        assertEquals(ParameterType.REPUTATION, third.approval().effects().getFirst().parameter());
+        assertEquals(MEDIUM_CHANGE, third.approval().effects().getFirst().delta()); 
     }
 
+    @Test
+    void deckLoader() throws IOException {
+        final Deck cardsDeck = new Deck();
+        final Card first = cardsDeck.getAllCards().get(0);
+        assertEquals(CharacterType.PROFESSOR, first.getCharacter());
+        assertEquals(ParameterType.PROFESSORS, first.getRefusal().getEffects().getFirst().getParameter());
+        assertEquals("No, the budget is too tight", first.getRefusal().getAnswer());
+        final Card third = cardsDeck.getAllCards().get(2);
+        assertEquals(CharacterType.MUM, third.getCharacter());
+        assertEquals(ParameterType.REPUTATION, third.getApproval().getEffects().getFirst().getParameter());
+        assertEquals(MEDIUM_CHANGE, third.getApproval().getEffects().getFirst().getDelta());
+    }
 }
