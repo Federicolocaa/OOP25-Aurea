963295d6c38c2e41f9e742b1907ba8040f4ddbbc
Nikita Piraino <nikita.piraino@studio.unibo.it>
2026-04-01 11:41:08 +0200

    this file is created to have a starting point in the code. I'm not sure if this will remain here. It now has a lot of comments, but was to have a common space from where the program starts.

---
+++ src/main/java/it/unibo/aurea/Main.java
@@ -0,0 +1,28 @@
+package it.unibo.aurea;
+
+import it.unibo.aurea.controller.GameControllerImpl;
+import it.unibo.aurea.controller.api.GameController;
+import it.unibo.aurea.model.GameConfigImpl;
+import it.unibo.aurea.model.GameEngineImpl;
+import it.unibo.aurea.model.api.GameConfig;
+import it.unibo.aurea.model.api.GameEngine;
+//import it.unibo.aurea.view.GameViewImpl;
+import it.unibo.aurea.view.api.GameView;
+
+public final class Main {
+
+    private Main() {
+    }
+
+    /**
+     * Avvia l'applicazione creando e collegando model, view e controller.
+     *
+     * @param args argomenti da linea di comando
+     */
+    public static void main(final String[] args) {
+        final GameConfig config = GameConfigImpl.createStandard();
+        //this sn't created already. final GameView view = new GameViewImpl(); //this sn't created already.
+        //final GameController controller=  GameControllerImpl(new GameEngineImpl(config), view);
+        //controller.startGame();
+    }
+}
