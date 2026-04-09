b78c7cb05a894309a2194167d61338ddd008540f
Nikita Piraino <nikita.piraino@studio.unibo.it>
2026-04-09 15:49:02 +0200

    started creating the file for the testing of gameClock

---
+++ src/test/java/it/unibo/aurea/GameClockTest.java
@@ -0,0 +1,15 @@
+package it.unibo.aurea;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import it.unibo.aurea.model.GameClockImpl;
+import it.unibo.aurea.model.GameConfigImpl;
+import it.unibo.aurea.model.api.GameClock;
+import it.unibo.aurea.model.api.GameConfig;
+
+public class GameClockTest {
+    
+}
